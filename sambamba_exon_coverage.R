d13de12f3b3ac187e81b48dda9d09e872e36dddf
Graeme Smith <graeme_c_smith@hotmail.com>
2019-11-22 10:50:32 +0000

    Refactored code

--- sambamba_exon_coverage.R
+++ sambamba_exon_coverage.R
@@ -4,23 +4,24 @@ library(plotly)
 library(magrittr)
 library(htmlwidgets)
 
-# Purpose: This script....
+# Purpose: This script takes the RAW output from sambamba and produces sum,mary tables and plots of the data
 
-# Usage:
-# Rscript ./chanjo_exon_coverage.R --args "/home/graeme/Desktop/NGS300_coverage"
+# Usage: Rscript chanjo_exon_coverage.R --args "/path_to_folder/exon_coverage"
 
 # Functions:
+
+# TODO refactor the two functions below into one with a 'simplify' boolean flag
 
 generate_coverage_plot <- function(df, panel) { 
   # Remove rows with NAs caused by regions not included between panels  
   df <- df[complete.cases(df), ]
   # Reorder the factors in region by median (Ensures the boxplots are plotted in order form lowest to highest)
-  df$region <- fct_reorder(df$region, df$meanCoverage,.fun = median, .desc = FALSE)
+  df$region <- fct_reorder(df$region, df$scaled_meanCoverage,.fun = median, .desc = FALSE)
   # Create a color palette to highlight samples by gene/transcript
   col=rainbow(length(levels(factor(df$gene))))[factor(df$gene)]
   # Plot coverage data (A series of boxplots showing coverage for each region, ordered by median)
   p <- df %>%
-    ggplot( aes(x=region, y=meanCoverage)
+    ggplot( aes(x=region, y=scaled_meanCoverage)
     ) +
     geom_boxplot(outlier.size = 0.5, aes(fill=gene)) +
     geom_jitter(color="grey", width = 0.01, size=1, alpha=0, shape = 1) +
@@ -34,15 +35,44 @@ generate_coverage_plot <- function(df, panel) {
   return(p)
 }
 
+generate_simple_coverage_plot <- function(df, panel) { 
+  # Remove rows with NAs caused by regions not included between panels  
+  df <- df[complete.cases(df), ]
+  # Group the tibble data structure by 'region'
+  df<- df %>% 
+    group_by(region)
+  region_mean <- df %>% 
+    summarise(region_meanCoverage = mean(scaled_meanCoverage))
+  # Order region factors by region_meanCoverage to produce plot in correct order
+  region_mean$region <- as.factor(region_mean$region)
+  region_mean$region <- fct_reorder(region_mean$region, region_mean$region_meanCoverage,.fun = median, .desc = FALSE)
+  # Plot region 
+  region_mean %>%
+    ggplot(aes(x=region, y=region_meanCoverage)) +
+    geom_point(col = "red", shape = 20, size = 0.1) + 
+    theme(
+      legend.position = "none",
+      plot.title = element_text(size=11),
+      axis.text.x = element_text(angle = 45, hjust = 1, size = 1)
+    ) +
+    ggtitle(paste("Run ", run_name,",  ", panel ,", Coverage over ", num_samples,"samples")) +
+    xlab("Target Region") +
+    ylab("Scaled average coverage")
+}
+
+# Uses scale() function on data - scaling is done by dividing the columns for each sample by their root mean square.
+# This allows easier comparison between samples.
+scale_this <- function(x) as.vector(scale(x, scale=TRUE, center = FALSE))
+
 # Main Script:
 
 # Get directory location from commandline - directory should contain the Raw exon level coverage files
 data_directory <- commandArgs(trailingOnly = TRUE)
-data_directory <- "/home/graeme/Desktop/NGS300_coverage"
-# Import all files with the suffix "*.bed" from data directory
+
+# Get all files with the suffix "*.bed" from data directory
 sambamba_files <- list.files(path = data_directory, pattern = "*.refined.sambamba_output.bed", full.names = TRUE)
 
-# Import coverage data
+# Import coverage data and add relevant sample ID to each imported row 
 tbl <- sapply(sambamba_files , read_tsv, col_types = "ciicicccinnc", simplify=FALSE) %>% 
   bind_rows(.id = "sample_id")
 
@@ -50,58 +80,64 @@ tbl <- sapply(sambamba_files , read_tsv, col_types = "ciicicccinnc", simplify=FA
 tbl$sample_id <- gsub(basename(tbl$sample_id), pattern = ".refined.sambamba_output.bed", replacement = "")
 # Rename 2nd column to remove proceding '#'
 colnames(tbl)[2] <- "chrom"
+# Replace F1:F6 labels with meaningful names
+# TODO find out what column F$ represents and rename accordingly
+colnames(tbl)[5:9] <- c("genomicCoordinates", "F4", "strand", "gene_transcript", "accessionNum")
 
-#  Convert dataframe to wide format:
-tbl$sample_id <- factor(tbl$sample_id)
-tbl <- tibble(sample_id = tbl$sample_id, 
-                      region = paste(tbl$chrom,
-                                     tbl$chromStart,
-                                     tbl$chromEnd,
-                                     tbl$F6,
-                                     sep=";"),
-                      meanCoverage = tbl$meanCoverage)
+# Add new column 'region' so that each target region is represented by unique ID
+tbl$region <- paste(tbl$chrom,
+                    tbl$chromStart,
+                    tbl$chromEnd,
+                    tbl$gene_transcript,
+                    sep=";")
 
-data_wide <- spread(tbl, sample_id, meanCoverage)
-
-# Use scale to centre the data - scaling is done by dividing the columns of x by their root mean square.
-data_wide[,-1] <- scale(data_wide[,-1], scale=TRUE, center = FALSE)
-
-# Cast back to long format for visualisation
-data_long <- gather(data_wide, sample, meanCoverage, 2:33)
+# Group the tibble data structure by 'region'
+tbl <- tbl %>% 
+  group_by(sample_id) %>%
+  mutate(scaled_meanCoverage = scale_this(meanCoverage))
 
 # Extract meta data from sample name
-run_name <- strsplit(data_long$sample, "_")[[1]][1]
+run_name <- strsplit(tbl$sample_id, "_")[[1]][1]
+# TODO: Add run ID to dataframe and add additional for loop below so that separate runs can be processed together
 
-# Split column
-data_long$region2 <- data_long$region
-data_long <- separate(data = data_long,
-                      col = region2,
-                      into = c("chrom", "chromStart", "chromEnd", "gene", "transcript"),
-                      sep = ";")
-data_long$gene[is.na(data_long$transcript)] <- "SNP" # Any SNPs referenced by their RS accession number will not have a transcript
-data_long$gene_transcript <- paste0(data_long$gene, ":", data_long$transcript)
-
+# Extract gene and transcript names into separate columns:
+tbl$gene <- stringr::str_split(string = tbl$gene_transcript, pattern = ";", simplify = TRUE)[,1]
+tbl$transcript <- stringr::str_split(string = tbl$gene_transcript, pattern = ";", simplify = TRUE)[,2]
+# Any SNPs referenced by their RS accession number will not have a transcript - label as 'dbSNP'
+tbl$gene[tbl$transcript==""] <- "dbSNP" 
 # Identify Pan number from sample name and add as additional column
-data_long$pan_number <- stringr::str_split(string = data_long$sample, pattern = "_", simplify = TRUE)[,7]
+tbl$pan_number <- stringr::str_split(string = tbl$sample_id, pattern = "_", simplify = TRUE)[,7]
+# Produce separate output for each panel
+for(panel in unique(tbl$pan_number)){
 
-for(panel in unique(data_long$pan_number)){
-
-  df <- data_long[data_long$pan_number==panel,]
+  df <- tbl[tbl$pan_number==panel,]
   
   # Update number of samples to be plotted 
-  num_samples <- length(unique(df$sample))
+  num_samples <- length(unique(df$sample_id))
   
   # Generate static plot of data for each
   static_plot <- generate_coverage_plot(df, panel)
   
-  # Add intactivity to plot:
+  # Add interactivity to plot:
   interactive_plot <- ggplotly(static_plot)
   
+  # Create coverage plot of means for PDF
+  
+  simplified_plot <- generate_simple_coverage_plot(df, panel)
+  
+  # TODO refactor filename generation to remove duplication
   # Save interactive plot as a single html file:
   filename <- paste0(run_name, "_", panel, "_coverage.html")
   saveWidget(ggplotly(interactive_plot), file = filename)
+  # Save simplified plot to pdf:
+  filename <- paste0(run_name, "_", panel, "_coverage.pdf")
+  ggsave(filename = filename, simplified_plot)
   # Save table
   filename <- paste0(run_name, "_", panel, "_coverage.csv")
   write_delim(df, filename, delim = "\t")
 }
+
+
+
+
 
